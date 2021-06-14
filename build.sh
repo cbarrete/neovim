a0ca240c5ff3c17ce12c8bae8c73c07aaa7ece5e
Cédric Barreteau <cbarrete@users.noreply.github.com>
2021-06-14 23:47:56 +0200

    build in release mode

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 rm -rf CMakeFiles CMakeCache.txt
 rm -r build/
-make -j CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/.local/share/neovim"
+make -j CMAKE_BUILD_TYPE=Release CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/.local/share/neovim"
 make install
